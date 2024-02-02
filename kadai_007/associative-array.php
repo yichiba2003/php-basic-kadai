8c8e82c8f1e01464cc3ff9113184d02f072558a6
yichiba2003 <150329674+yichiba2003@users.noreply.github.com>
2024-02-01 18:52:28 -0800

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+    
+    <?php
+    $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($personal_data);
+    ?>
+
+</body>
+
+</html>
+
